4c142e07baef30a2b2d89e75f0a8878b37983486
benedict <sridamian@gmail.com>
2024-03-26 22:16:49 +0000

    add templates

--- config/urls.py
+++ config/urls.py
@@ -7,6 +7,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
      path('backend/', include('backend.urls')),
+     path('', include('frontend.urls')),
+     
       #path('backend/', include('djoser.urls')),
        #path('backend/', include('djoser.urls.authtoken')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- frontend/views.py
+++ frontend/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home_view(request,*args,**kwargs):
+    context={}
+    return render(request,'./frontend/pages/index.html',context) 
